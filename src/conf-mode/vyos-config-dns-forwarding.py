d9c550c2051fa44a3a09cc2d029cc183b07cee72
Christian Poessinger <christian@poessinger.com>
2018-03-16 20:09:59 +0100

    Initial CLI interface support for XML DNS forwarder

---
+++ src/conf-mode/vyos-config-dns-forwarding.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2018 VyOS maintainers and contributors
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 or later as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+
+import sys
+import os
+
+from vyos.config import Config
+from vyos.util import ConfigError
+
+def get_config():
+    return None
+
+def verify(dns):
+    return None
+
+def generate(dns):
+    return None
+
+def apply(dns):
+    return None
+
+if __name__ == '__main__':
+    try:
+        c = get_config()
+        verify(c)
+        generate(c)
+        apply(c)
+    except ConfigError as e:
+        print(e)
+        sys.exit(1)
