a18d5f06a149e9852d89f751c1df512483e17763
Christian Poessinger <christian@poessinger.com>
2018-06-30 22:19:10 +0200

    src: commands-pipe: fix bug (no raw string) reported by sonarcube

--- src/helpers/commands-pipe.py
+++ src/helpers/commands-pipe.py
@@ -8,7 +8,6 @@ from vyos.configtree import ConfigTree
 
 signal(SIGPIPE,SIG_DFL)
 
-
 config_string = sys.stdin.read().strip()
 
 if not config_string:
@@ -21,7 +20,7 @@ if not config_string:
 # __root__ is hygienic enough.
 config_string = "__root__ {{ {0} \n }}".format(config_string)
 
-config_re = re.compile("(set|comment)\s+__root__\s+(.*)")
+config_re = re.compile(r'(set|comment)\s+__root__\s+(.*)')
 
 config = ConfigTree(config_string)
 commands = config.to_commands()
