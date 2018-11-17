33ed35f45766d9157a25a7591a4ebe8a9a67be8e
Christian Poessinger <christian@poessinger.com>
2018-11-17 11:03:08 +0100

    T1016: fix IPv4/IPv6 dhcp relay restart command
    Current implementation referred to a no longer existing Perl script to
    restart the IPv4 and IPv6 instance of dhcrelay.
    
    > restart dhcp relay-agent
    > restart dhcpv6 relay-agent
    
    (cherry picked from commit f9007c5f5e6e7957ab1c955c10b3842dca7b253d)

---
+++ src/op_mode/restart_dhcp_relay.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2018 VyOS maintainers and contributors
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 or later as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# File: restart_dhcp_relay.py
+# Purpose:
+#    Restart IPv4 and IPv6 DHCP relay instances of dhcrelay service
+
+import sys
+import argparse
+import os
+
+import vyos.config
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--ipv4", action="store_true", help="Restart IPv4 DHCP relay")
+parser.add_argument("--ipv6", action="store_true", help="Restart IPv6 DHCP relay")
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    c = vyos.config.Config()
+
+    if args.ipv4:
+        # Do nothing if service is not configured
+        if not c.exists_effective('service dhcp-relay'):
+            print("DHCP relay service not configured")
+        else:
+            os.system('sudo systemctl restart isc-dhcp-relay.service')
+
+        sys.exit(0)
+    elif args.ipv6:
+        # Do nothing if service is not configured
+        if not c.exists_effective('service dhcpv6-relay'):
+            print("DHCPv6 relay service not configured")
+        else:
+            os.system('sudo systemctl restart isc-dhcpv6-relay.service')
+
+        sys.exit(0)
+    else:
+        parser.print_help()
+        sys.exit(1)
