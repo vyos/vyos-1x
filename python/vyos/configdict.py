2ee0940e4d0d3094a2526401e6aa99e6a3058ec2
Daniil Baturin <daniil@baturin.org>
2019-01-02 23:51:04 +0100

    Initial implementation of declarative config dict retrieval library.

---
+++ python/vyos/configdict.py
@@ -0,0 +1,80 @@
+# Copyright 2019 VyOS maintainers and contributors <maintainers@vyos.io>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+A library for retrieving value dicts from VyOS configs in a declarative fashion.
+
+"""
+
+
+def retrieve_config(path_hash, base_path, config):
+    """
+    Retrieves a VyOS config as a dict according to a declarative description
+
+    The description dict, passed in the first argument, must follow this format:
+    ``field_name : <path, type, [inner_options_dict]>``.
+
+    Supported types are: ``str`` (for normal nodes),
+    ``list`` (returns a list of strings, for multi nodes),
+    ``bool`` (returns True if valueless node exists),
+    ``dict`` (for tag nodes, returns a dict indexed by node names,
+    according to description in the third item of the tuple).
+
+    Args:
+        path_hash (dict): Declarative description of the config to retrieve
+        base_path (list): A base path to prepend to all option paths
+        config (vyos.config.Config): A VyOS config object
+
+    Returns:
+        dict: config dict
+    """
+    config_hash = {}
+
+    for k in path_hash:
+
+        if type(path_hash[k]) != tuple:
+            raise ValueError("In field {0}: expected a tuple, got a value {1}".format(k, str(path_hash[k])))
+        if len(path_hash[k]) < 2:
+            raise ValueError("In field {0}: field description must be a tuple of at least two items, path (list) and type".format(k))
+
+        path = path_hash[k][0]
+        if type(path) != list:
+            raise ValueError("In field {0}: path must be a list, not a {1}".format(k, type(path)))
+
+        typ = path_hash[k][1]
+        if type(typ) != type:
+            raise ValueError("In field {0}: type must be a type, not a {1}".format(k, type(typ)))
+
+        path = base_path + path
+
+        path_str = " ".join(path)
+
+        if typ == str:
+            config_hash[k] = config.return_value(path_str)
+        elif typ == list:
+            config_hash[k] = config.return_values(path_str)
+        elif typ == bool:
+            config_hash[k] = config.exists(path_str)
+        elif typ == dict:
+            try:
+                inner_hash = path_hash[k][2]
+            except IndexError:
+                raise ValueError("The type of the \'{0}\' field is dict, but inner options hash is missing from the tuple".format(k))
+            config_hash[k] = {}
+            nodes = config.list_nodes(path_str)
+            for node in nodes:
+                config_hash[k][node] = retrieve_config(inner_hash, path + [node], config)
+
+    return config_hash
