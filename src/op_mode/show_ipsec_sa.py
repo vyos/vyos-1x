2f67ed3a142f74be43b0fd7e6cf8e7717b2c1b06
Daniel Pro <43214013+daniel-pro@users.noreply.github.com>
2019-02-21 15:13:17 +0100

    T1256: Execute "show ipsec vpn ipsec sa" returns incorrect results
    Not sure it's a normal case scenario, the one highlighted in T1256.
    To managed it I changed the "if" logic.

--- src/op_mode/show_ipsec_sa.py
+++ src/op_mode/show_ipsec_sa.py
@@ -67,7 +67,7 @@ status_data = []
 
 for conn in connections:
     status = subprocess.check_output("ipsec statusall {0}".format(conn), shell=True).decode()
-    if re.search(r'no match|CONNECTING', status):
+    if not re.search(r'ESTABLISHED', status):
         status_line = [conn, "down", None, None, None, None, None]
     else:
         try:
