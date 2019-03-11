b72a88200effbc7f94dfce3ba0db02b8a2e1b077
hagbard <vyosdev@derith.de>
2019-03-11 13:44:17 -0700

    [arp] - T1288: python implemtation of 'set protocols static arp'

---
+++ src/conf_mode/arp.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2018 VyOS maintainers and contributors
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 or later as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+
+import sys
+import os
+import re
+import syslog as sl
+import subprocess
+
+from vyos.config import Config
+from vyos import ConfigError
+
+arp_cmd = '/usr/sbin/arp'
+
+def get_config():
+  c = Config()
+  if not c.exists('protocols static arp'):
+    return None
+
+  c.set_level('protocols static')
+  config_data = {}
+  
+  for ip_addr in c.list_nodes('arp'):
+    config_data.update(
+        {
+          ip_addr : c.return_value('arp ' + ip_addr + ' hwaddr')
+        }
+    )
+
+  return config_data
+
+def generate(c):
+  c_eff = Config()
+  c_eff.set_level('protocols static')
+  c_eff_cnf = {}
+  for ip_addr in c_eff.list_effective_nodes('arp'):
+    c_eff_cnf.update(
+        {
+          ip_addr : c_eff.return_effective_value('arp ' + ip_addr + ' hwaddr')
+        }
+    )
+
+  config_data = {
+    'remove'  : [],
+    'update'  : {}
+  }
+  ### removal
+  if c == None:
+    for ip_addr in c_eff_cnf:
+      config_data['remove'].append(ip_addr)
+  else:
+    for ip_addr in c_eff_cnf:
+      if not ip_addr in c:
+        config_data['remove'].append(ip_addr)
+
+  ### add/update
+  if c != None:
+    for ip_addr in c:
+      if not ip_addr in c_eff_cnf:
+        config_data['update'][ip_addr] = c[ip_addr]
+      if  ip_addr in c_eff_cnf:
+        if c[ip_addr] != c_eff_cnf[ip_addr]:
+          config_data['update'][ip_addr] = c[ip_addr]
+
+  return config_data
+
+def apply(c):
+  for ip_addr in c['remove']:
+    sl.syslog(sl.LOG_NOTICE, "arp -d " + ip_addr)
+    subprocess.call([arp_cmd + ' -d ' + ip_addr + ' >/dev/null 2>&1'], shell=True)
+
+  for ip_addr in c['update']:
+    sl.syslog(sl.LOG_NOTICE, "arp -s " + ip_addr + " " + c['update'][ip_addr])
+    subprocess.call([arp_cmd + ' -s ' + ip_addr + ' ' + c['update'][ip_addr] ], shell=True)
+
+
+if __name__ == '__main__':
+  try:
+    c = get_config()
+    ## syntax verification is done via cli
+    config = generate(c)
+    apply(config)
+  except ConfigError as e:
+    print(e)
+    sys.exit(1)
