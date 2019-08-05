9ac783472872329fe1a1683585b679a7afcc78f0
Daniil Baturin <daniil@baturin.org>
2019-08-05 21:20:54 +0200

    T1431: add showConfig operation to the HTTP API.

--- python/vyos/configsession.py
+++ python/vyos/configsession.py
@@ -23,6 +23,7 @@ DELETE = '/opt/vyatta/sbin/my_delete'
 COMMENT = '/opt/vyatta/sbin/my_comment'
 COMMIT = '/opt/vyatta/sbin/my_commit'
 DISCARD = '/opt/vyatta/sbin/my_discard'
+SHOW_CONFIG = ['/bin/cli-shell-api', 'showConfig']
 
 # Default "commit via" string
 APP = "vyos-http-api"
@@ -147,3 +148,10 @@ class ConfigSession(object):
 
     def discard(self):
         self.__run_command([DISCARD])
+
+    def show_config(self, path, format='raw'):
+        config_data = self.__run_command(SHOW_CONFIG + path)
+
+        if format == 'raw':
+            return config_data
+
