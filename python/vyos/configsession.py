86d1291ec5323e921a0f995211e223bf1f7dcdff
John Estabrook <jestabro@sentrium.io>
2019-08-31 17:25:07 -0500

    [boot-config-loader] T1622: Add failsafe and back trace

--- python/vyos/configsession.py
+++ python/vyos/configsession.py
@@ -24,6 +24,7 @@ COMMENT = '/opt/vyatta/sbin/my_comment'
 COMMIT = '/opt/vyatta/sbin/my_commit'
 DISCARD = '/opt/vyatta/sbin/my_discard'
 SHOW_CONFIG = ['/bin/cli-shell-api', 'showConfig']
+LOAD_CONFIG = ['/bin/cli-shell-api', 'loadFile']
 
 # Default "commit via" string
 APP = "vyos-http-api"
@@ -155,3 +156,5 @@ class ConfigSession(object):
         if format == 'raw':
             return config_data
 
+    def load_config(self, file_path):
+        self.__run_command(LOAD_CONFIG + [file_path])
