bc36bb5a9db4143696b96ab74eee40539de3541d
Christian Poessinger <christian@poessinger.com>
2019-12-25 17:51:29 +0100

    time-zone: T1906: migrate to XML/Python
    The current node.def based implementtion should be migrated from
    vyatta-cfg-system to vyos-1x. During the migration also provide a migration
    script which transforms some ole timezones like "Los_Angeles" into a proper
    IANA assigned timezone which should be "America/Los_Angeles".

---
+++ src/conf_mode/timezone.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2019 VyOS maintainers and contributors
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 or later as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+import os
+
+from copy import deepcopy
+from vyos.config import Config
+from vyos import ConfigError
+
+default_config_data = {
+    'name': 'UTC'
+}
+
+def get_config():
+    tz = deepcopy(default_config_data)
+    conf = Config()
+    if conf.exists('system time-zone'):
+        tz['name'] = conf.return_value('system time-zone')
+
+    return tz
+
+def verify(tz):
+    pass
+
+def generate(tz):
+    pass
+
+def apply(tz):
+    cmd = '/usr/bin/timedatectl set-timezone {}'.format(tz['name'])
+    os.system(cmd)
+    pass
+
+if __name__ == '__main__':
+    try:
+        c = get_config()
+        verify(c)
+        generate(c)
+        apply(c)
+    except ConfigError as e:
+        print(e)
+        sys.exit(1)
