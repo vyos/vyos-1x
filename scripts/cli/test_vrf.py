e978f00f4749866a38c349528360a063b47d78ff
Christian Poessinger <christian@poessinger.com>
2020-03-05 22:08:49 +0100

    vrf: initial unittest

---
+++ scripts/cli/test_vrf.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2020 VyOS maintainers and contributors
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 or later as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import unittest
+
+from vyos.configsession import ConfigSession, ConfigSessionError
+from vyos.util import read_file
+
+class VRFTest(unittest.TestCase):
+    def setUp(self):
+        self.session = ConfigSession(os.getpid())
+        self._vrfs = ['red', 'green', 'blue']
+
+    def tearDown(self):
+        # delete all VRFs
+        self.session.delete(['vrf'])
+        self.session.commit()
+        del self.session
+
+    def test_table_id(self):
+        table = 1000
+        for vrf in self._vrfs:
+            base = ['vrf', 'name', vrf]
+            description = "VyOS-VRF-" + vrf
+            self.session.set(base + ['description', description])
+
+            # check validate() - a table ID is mandatory
+            with self.assertRaises(ConfigSessionError):
+                self.session.commit()
+
+            self.session.set(base + ['table', str(table)])
+            table += 1
+
+        # commit changes
+        self.session.commit()
+
+if __name__ == '__main__':
+    unittest.main()
