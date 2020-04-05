b964713c491fcf7d5a3323d1d187415c5b650940
Thomas Mangin <thomas.mangin@exa.net.uk>
2020-04-05 16:31:15 +0100

    util: T2226: rewrite list_dumpable_interfaces to use cmd

--- src/completion/list_dumpable_interfaces.py
+++ src/completion/list_dumpable_interfaces.py
@@ -3,12 +3,10 @@
 # Extract the list of interfaces available for traffic dumps from tcpdump -D
 
 import re
-import subprocess
+
+from vyos.util import cmd
 
 if __name__ == '__main__':
-    out = subprocess.check_output(['/usr/sbin/tcpdump', '-D']).decode().strip()
-    out = out.split("\n")
-
+    out = cmd('/usr/sbin/tcpdump -D').split('\n')
     intfs = " ".join(map(lambda s: re.search(r'\d+\.(\S+)\s', s).group(1), out))
-
     print(intfs)
