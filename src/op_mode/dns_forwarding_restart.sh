0ef026f32945ca6408026617696b9a96dd000295
Christian Poessinger <christian@poessinger.com>
2020-04-13 20:21:50 +0200

    op-mode: dns-forwarding: T2185: migrate from SysVinit to systemd

--- src/op_mode/dns_forwarding_restart.sh
+++ src/op_mode/dns_forwarding_restart.sh
@@ -2,7 +2,7 @@
 
 if cli-shell-api existsEffective service dns forwarding; then
     echo "Restarting the DNS forwarding service"
-    systemctl restart pdns-recursor
+    systemctl restart pdns-recursor.service
 else
     echo "DNS forwarding is not configured"
 fi
