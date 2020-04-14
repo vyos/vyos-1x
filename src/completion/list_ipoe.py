603d47d5e2cfc9b72a6de689136231115a3e0d6f
DmitriyEshenko <dmitriy.eshenko@vyos.io>
2020-04-14 18:42:54 +0000

    ipoe: T2256: Rewrite op-mode commands

---
+++ src/completion/list_ipoe.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import argparse
+from vyos.util import popen
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--selector', help='Selector: username|ifname|sid', required=True)
+    args = parser.parse_args()
+
+    output, err = popen("accel-cmd -p 2002 show sessions {0}".format(args.selector))
+    if not err:
+        res = output.split("\r\n")
+        # Delete header from list
+        del res[:2]
+        print(' '.join(res))
