6eda5c820f4d4d98920a0b45aaac37c8a88d40e7
Christian Poessinger <christian@poessinger.com>
2020-04-16 18:07:27 +0200

    T2305: include release train in "show version" output
    (cherry picked from commit b4544f196c2beededdd76f581690b1490c755dac)

--- src/op_mode/version.py
+++ src/op_mode/version.py
@@ -49,6 +49,8 @@ def read_file(name):
 
 version_output_tmpl = """
 Version:          VyOS {{version}}
+Release Train:    {{release_train}}
+
 Built by:         {{built_by}}
 Built on:         {{built_on}}
 Build UUID:       {{build_uuid}}
@@ -98,7 +100,7 @@ if __name__ == '__main__':
     elif subprocess.call(""" grep '^overlay /' /proc/mounts >/dev/null """, shell=True) != 0:
         boot_via = "legacy non-image installation"
     version_data['boot_via'] = boot_via
-    
+
 
     # Get hardware details from DMI
     version_data['hardware_vendor'] = read_file('/sys/class/dmi/id/sys_vendor')
