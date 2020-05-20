2e8bd0ced8967644b0ad361df9b375075276593a
Christian Poessinger <christian@poessinger.com>
2020-05-20 21:09:31 +0200

    ifconfig: T2023: add initial MACsec abstraction

--- python/vyos/ifconfig/__init__.py
+++ python/vyos/ifconfig/__init__.py
@@ -42,3 +42,4 @@ from vyos.ifconfig.tunnel import SitIf
 from vyos.ifconfig.tunnel import Sit6RDIf
 from vyos.ifconfig.wireless import WiFiIf
 from vyos.ifconfig.l2tpv3 import L2TPv3If
+from vyos.ifconfig.macsec import MACsecIf
