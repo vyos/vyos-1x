c3d68789dd4c671187a808b82ce73245f0e99282
Geoff Adams <gadams@avernus.com>
2020-05-28 02:11:33 -0700

    dhcpv6-pd: T421: Start up dhcp6c last for prefix delegation to prevent a race

--- src/conf_mode/interfaces-ethernet.py
+++ src/conf_mode/interfaces-ethernet.py
@@ -294,13 +294,15 @@ def apply(eth):
         if eth['is_bridge_member']:
             e.add_to_bridge(eth['is_bridge_member'])
 
-        # If DHCPv6-PD is requested, and the IPv6 addresses are not being set
-        # via DHCPv6, then we still need to start dhcp6c.
-        if eth['dhcpv6_pd_interfaces'] and 'dhcpv6' not in eth['address']:
-            e.request_prefix_delegation()
-
         # apply all vlans to interface
         apply_all_vlans(e, eth)
+
+        # If DHCPv6-PD is requested, and the IPv6 addresses are not being set
+        # via DHCPv6, then we still need to start dhcp6c. Do this after
+        # apply_all_vlans in case we are delegating to a vlan; in that case,
+        # dhcp6c would fail to start, since its config woula appear invalid.
+        if eth['dhcpv6_pd_interfaces'] and 'dhcpv6' not in eth['address']:
+            e.request_prefix_delegation()
 
     return None
 
