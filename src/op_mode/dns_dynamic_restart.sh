84ef5431981c1b434b42053f6305a1e4acae754f
Christian Poessinger <christian@poessinger.com>
2020-05-29 19:59:13 +0200

    Revert "dynamic-dns: op-mode: add restart command"
    This is actually implemented as "update dns dynamic"
    
    This reverts commit 968ea6284fe177aa0c3ace747e2027d151e2474e.

--- src/op_mode/dns_dynamic_restart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if cli-shell-api existsEffective service dns dynamic; then
-    echo "Restarting dynamic DNS service"
-    systemctl restart ddclient.service
-else
-    echo "Dynamic DNS update service is not configured"
-fi
