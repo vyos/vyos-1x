b38a42d9d4ab302b44d48844fae49bb0a0817d04
Christian Poessinger <christian@poessinger.com>
2020-06-19 21:29:04 +0200

    peth/macvlan: T2589: add basic smoketest

---
+++ scripts/cli/test_interfaces_pseudo_ethernet.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2020 VyOS maintainers and contributors
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 or later as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+
+from base_interfaces_test import BasicInterfaceTest
+
+class PEthInterfaceTest(BasicInterfaceTest.BaseTest):
+
+    def setUp(self):
+         super().setUp()
+         self._base_path = ['interfaces', 'pseudo-ethernet']
+         self._options = {
+             'peth0': ['source-interface eth0', 'ip arp-cache-timeout 10'],
+             'peth1': ['source-interface eth0', 'ip arp-cache-timeout 10'],
+         }
+         self._interfaces = list(self._options)
+
+if __name__ == '__main__':
+    unittest.main()
