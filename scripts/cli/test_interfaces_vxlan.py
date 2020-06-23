82024ebc21ff8516620f2fc8ee47e62f83082e3a
Christian Poessinger <christian@poessinger.com>
2020-06-23 08:14:50 +0200

    vxlan: adjust to new CLI source-interface node

--- scripts/cli/test_interfaces_vxlan.py
+++ scripts/cli/test_interfaces_vxlan.py
@@ -14,12 +14,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import os
 import unittest
 
 from vyos.configsession import ConfigSession, ConfigSessionError
 from base_interfaces_test import BasicInterfaceTest
-
 
 class VXLANInterfaceTest(BasicInterfaceTest.BaseTest):
     def setUp(self):
@@ -29,7 +27,7 @@ class VXLANInterfaceTest(BasicInterfaceTest.BaseTest):
         self._base_path = ['interfaces', 'vxlan']
         self._options = {
             'vxlan0': ['vni 10', 'remote 127.0.0.2'],
-            'vxlan1': ['vni 20', 'group 239.1.1.1', 'link eth0'],
+            'vxlan1': ['vni 20', 'group 239.1.1.1', 'source-interface eth0'],
         }
         self._interfaces = list(self._options)
 
