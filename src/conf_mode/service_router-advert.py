vyos/vyos-1x

commit 740ace13f36b78583434eabfbd324cf3d29e909d

router-advert: T2609: add missing verify() for prefix lifetime
radvd[31898]: AdvValidLifeTime must be greater than AdvPreferredLifetime in
radvd.conf, line 19

This happens with the following configuration:
  vyos@vyos# show service router-advert
   interface eth0.20 {
       name-server 2001:4860:4860::8888
       prefix ::/64 {
           valid-lifetime 7200
       }
   }

A validator is added to solve this issue and radvd will run again.

src/conf_mode/service_router-advert.py
@@ -107,8 +107,8 @@ def get_config():
                 'prefix' : prefix,
                 'autonomous_flag' : 'on',
                 'on_link' : 'on',
-                'preferred_lifetime': '14400',
-                'valid_lifetime' : '2592000'
+                'preferred_lifetime': 14400,
+                'valid_lifetime' : 2592000
 
             }
 
@@ -122,10 +122,10 @@ def get_config():
                 tmp['on_link'] = 'off'
 
             if conf.exists(['preferred-lifetime']):
-                tmp['preferred_lifetime'] = conf.return_value(['preferred-lifetime'])
+                tmp['preferred_lifetime'] = int(conf.return_value(['preferred-lifetime']))
 
             if conf.exists(['valid-lifetime']):
-                tmp['valid_lifetime'] = conf.return_value(['valid-lifetime'])
+                tmp['valid_lifetime'] = int(conf.return_value(['valid-lifetime']))
 
             intf['prefixes'].append(tmp)
 
@@ -134,6 +134,13 @@ def get_config():
     return rtradv
 
 def verify(rtradv):
+    for interface in rtradv['interfaces']:
+        for prefix in interface['prefixes']:
+            import pprint
+            pprint.pprint(prefix)
+            if not (prefix['valid_lifetime'] > prefix['preferred_lifetime']):
+                raise ConfigError('Prefix valid-lifetime must be greater then preferred-lifetime')
+
     return None
 
 def generate(rtradv):

data/templates/router-advert/radvd.conf.tmpl
@@ -1,4 +1,4 @@
-### Autogenerated by service-router-advert.py ###
+### Autogenerated by service_router-advert.py ###
 
 {% for i in interfaces -%}
 interface {{ i.name }} {

commit 3658058c6299a107dd5ce5973a9ec5e4be7064b0

router-advert: T2758: bugfix 'infinity' is not a valid integer number
Problem was introduced in commit 740ace13 ("router-advert: T2609: add missing
verify() for prefix lifetime"). The 'infinity' literal was not converted to its
corresponding integer 0xffffffff (4294967295) for the check.

src/conf_mode/service_router-advert.py
@@ -73,7 +73,15 @@ def verify(rtradv):
         if 'prefix' in interface:
             for prefix in interface['prefix']:
                 prefix = interface['prefix'][prefix]
-                if not (int(prefix['valid_lifetime']) > int(prefix['preferred_lifetime'])):
+                valid_lifetime = prefix['valid_lifetime']
+                if valid_lifetime == 'infinity':
+                    valid_lifetime = 4294967295
+
+                preferred_lifetime = prefix['preferred_lifetime']
+                if preferred_lifetime == 'infinity':
+                    preferred_lifetime = 4294967295
+
+                if not (int(valid_lifetime) > int(preferred_lifetime)):
                     raise ConfigError('Prefix valid-lifetime must be greater then preferred-lifetime')
 
     return None