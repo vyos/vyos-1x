c8ea23cc3d3cab6e384b143d7d22030a9085f1a2
Christian Poessinger <christian@poessinger.com>
2020-08-23 11:47:30 +0200

    Revert "ifconfig: T2653: drop unused vyos.ifconfig.pppoe"
    This reverts commit 5a5974d5a00b482cabd3dee92bc365d3c9f399bc.
    
    Required for operational mode "show interfaces" command.

--- python/vyos/ifconfig/__init__.py
+++ python/vyos/ifconfig/__init__.py
@@ -30,6 +30,7 @@ from vyos.ifconfig.vxlan import VXLANIf
 from vyos.ifconfig.wireguard import WireGuardIf
 from vyos.ifconfig.vtun import VTunIf
 from vyos.ifconfig.vti import VTIIf
+from vyos.ifconfig.pppoe import PPPoEIf
 from vyos.ifconfig.tunnel import GREIf
 from vyos.ifconfig.tunnel import GRETapIf
 from vyos.ifconfig.tunnel import IP6GREIf
