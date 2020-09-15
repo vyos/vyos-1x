40ca599350731a743a0d999205df10829017a783
Christian Poessinger <christian@poessinger.com>
2020-09-15 18:55:46 +0200

    completion: T2238: add license

--- src/completion/list_interfaces.py
+++ src/completion/list_interfaces.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python3
+#
+# Copyright (C) 2019-2020 VyOS maintainers and contributors
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 or later as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys
 import argparse
 from vyos.ifconfig import Section
 
-
 def matching(feature):
     for section in Section.feature(feature):
         for intf in Section.interfaces(section):
             yield intf
-
 
 parser = argparse.ArgumentParser()
 group = parser.add_mutually_exclusive_group()
