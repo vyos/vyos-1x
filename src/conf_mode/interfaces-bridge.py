f0400b7e24944de75fef8cdd0d7eb016b224b9c7
Christian Poessinger <christian@poessinger.com>
2020-09-26 12:38:19 +0200

    ifconfig: mtu: T2928: remove bridge mtu check as our bridge interface has no mtu

--- src/conf_mode/interfaces-bridge.py
+++ src/conf_mode/interfaces-bridge.py
@@ -25,7 +25,6 @@ from vyos.configdict import node_changed
 from vyos.configdict import is_member
 from vyos.configdict import is_source_interface
 from vyos.configverify import verify_dhcpv6
-from vyos.configverify import verify_mtu_ipv6
 from vyos.configverify import verify_vrf
 from vyos.ifconfig import BridgeIf
 from vyos.validate import has_address_configured
@@ -96,7 +95,6 @@ def verify(bridge):
     if 'deleted' in bridge:
         return None
 
-    verify_mtu_ipv6(bridge)
     verify_dhcpv6(bridge)
     verify_vrf(bridge)
 
