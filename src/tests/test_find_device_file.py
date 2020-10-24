3aa70278b1697aab72aa6a5e1a9ac4aaad3acefe
Christian Poessinger <christian@poessinger.com>
2020-10-24 17:05:10 +0200

    test: vyos.util: add testcase for find_device_file()

---
+++ src/tests/test_find_device_file.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2020 VyOS maintainers and contributors
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 or later as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+from unittest import TestCase
+
+from vyos.util import find_device_file
+
+class TestDeviceFile(TestCase):
+    """ used to find USB devices on target """
+    def setUp(self):
+        pass
+
+    def test_null(self):
+        self.assertEqual(find_device_file('null'), '/dev/null')
+
+    def test_zero(self):
+        self.assertEqual(find_device_file('zero'), '/dev/zero')
+
+    def test_input_event(self):
+        self.assertEqual(find_device_file('event0'), '/dev/input/event0')
+
+    def test_non_existing(self):
+        self.assertFalse(find_device_file('vyos'))
