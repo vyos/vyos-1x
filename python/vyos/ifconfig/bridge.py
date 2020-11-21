9b8e3d83e9cf106e2203300e1d5b940f839c9827
JACK <jack9603301@163.com>
2020-11-21 16:25:08 +0800

    bridge: T3079: bugfix on VLAN 1 is deleted in VLAN-aware bridges

--- python/vyos/ifconfig/bridge.py
+++ python/vyos/ifconfig/bridge.py
@@ -318,15 +318,19 @@ class BridgeIf(Interface):
                 
                 if 'allowed_vlan' in interface_config:
                     vlan_filter = 1
-                    for vlan in interface_config['allowed_vlan']:
-                        cmd = f'bridge vlan add dev {interface} vid {vlan} master'
-                        self._cmd(cmd)
-                        vlan_add.add(vlan)
                 
                 if vlan_filter:
                     if 'native_vlan' not in interface_config:
                         cmd = f'bridge vlan del dev {interface} vid 1'
                         self._cmd(cmd)
+                
+                if 'allowed_vlan' in interface_config:
+                    for vlan in interface_config['allowed_vlan']:
+                        cmd = f'bridge vlan add dev {interface} vid {vlan} master'
+                        self._cmd(cmd)
+                        vlan_add.add(vlan)
+                
+                
         
         
         for vlan in vlan_del:
