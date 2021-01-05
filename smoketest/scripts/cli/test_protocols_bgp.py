c4c6fcb45e67685da2f1db273bf590c8fb147cb3
Christian Poessinger <christian@poessinger.com>
2021-01-05 16:16:01 +0100

    Revert "smoketest: bgp: enable description test"
    This reverts commit e7dd8c17d527aeb31b93b966c59a311f7506b906.

--- smoketest/scripts/cli/test_protocols_bgp.py
+++ smoketest/scripts/cli/test_protocols_bgp.py
@@ -44,7 +44,8 @@ neighbor_config = {
         'cap_strict'  : '',
         },
     '192.0.2.3' : {
-        'description' : 'foo bar baz',
+#        XXX: not available in current Perl backend
+#       'description' : 'foo bar baz',
         'remote_as'   : '200',
         'passive'     : '',
         'multi_hop'   : '5',
