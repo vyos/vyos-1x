42f91ee461589f1a407a2ceec8c63dd889f4a609
Christian Poessinger <christian@poessinger.com>
2021-01-24 13:14:48 +0100

    ospfv3: T3244: provide full protocol support in XML and Python
    This commit provides the implementation of the OSPFv3 (IPv6) CLI with a Jinja2
    template that is loaded by FRR reload. It also contains some initial smoketests.
    
    There is yet no verify() implementation!

---
+++ src/conf_mode/protocols_ospfv3.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2021 VyOS maintainers and contributors
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 or later as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+
+from sys import exit
+
+from vyos.config import Config
+from vyos.configdict import dict_merge
+from vyos.template import render
+from vyos.template import render_to_string
+from vyos.util import call
+from vyos.util import dict_search
+from vyos.xml import defaults
+from vyos import ConfigError
+from vyos import frr
+from vyos import airbag
+airbag.enable()
+
+config_file = r'/tmp/ospfv3.frr'
+frr_daemon = 'ospf6d'
+
+DEBUG = os.path.exists('/tmp/ospfv3.debug')
+if DEBUG:
+    import logging
+    lg = logging.getLogger("vyos.frr")
+    lg.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    lg.addHandler(ch)
+
+def get_config(config=None):
+    if config:
+        conf = config
+    else:
+        conf = Config()
+    base = ['protocols', 'ospfv3']
+    ospfv3 = conf.get_config_dict(base, key_mangling=('-', '_'), get_first_key=True)
+    return ospfv3
+
+def verify(ospfv3):
+    if not ospfv3:
+        return None
+
+    return None
+
+def generate(ospfv3):
+    if not ospfv3:
+        ospfv3['new_frr_config'] = ''
+        return None
+
+    # render(config) not needed, its only for debug
+    render(config_file, 'frr/ospfv3.frr.tmpl', ospfv3)
+    ospfv3['new_frr_config'] = render_to_string('frr/ospfv3.frr.tmpl', ospfv3)
+
+    return None
+
+def apply(ospfv3):
+    # Save original configuration prior to starting any commit actions
+    frr_cfg = frr.FRRConfig()
+    frr_cfg.load_configuration(frr_daemon)
+    frr_cfg.modify_section('router ospf6', '')
+    frr_cfg.add_before(r'(ip prefix-list .*|route-map .*|line vty)', ospfv3['new_frr_config'])
+
+    # Debugging
+    if DEBUG:
+        from pprint import pprint
+        print('')
+        print('--------- DEBUGGING ----------')
+        pprint(dir(frr_cfg))
+        print('Existing config:\n')
+        for line in frr_cfg.original_config:
+            print(line)
+        print(f'Replacement config:\n')
+        print(f'{ospfv3["new_frr_config"]}')
+        print(f'Modified config:\n')
+        print(f'{frr_cfg}')
+
+    frr_cfg.commit_configuration(frr_daemon)
+
+    # If FRR config is blank, re-run the blank commit x times due to frr-reload
+    # behavior/bug not properly clearing out on one commit.
+    if ospfv3['new_frr_config'] == '':
+        for a in range(5):
+            frr_cfg.commit_configuration(frr_daemon)
+
+    return None
+
+if __name__ == '__main__':
+    try:
+        c = get_config()
+        verify(c)
+        generate(c)
+        apply(c)
+    except ConfigError as e:
+        print(e)
+        exit(1)
