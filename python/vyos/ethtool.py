73c5ef5b91ff0c03fafbcef9f195e479987cc170
Christian Poessinger <christian@poessinger.com>
2021-02-20 20:41:53 +0100

    vyos.ethtool: import helper class
    This helper class could be used to interact and retrieve information from
    ethtool. It is not used so far in production code.

---
+++ python/vyos/ethtool.py
@@ -0,0 +1,80 @@
+# Copyright 2021 VyOS maintainers and contributors <maintainers@vyos.io>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+from vyos.util import cmd
+
+class Ethtool:
+    """
+    Class is used to retrive and cache information about an ethernet adapter
+    """
+
+    # dictionary containing driver featurs, it will be populated on demand and
+    # the content will look like:
+    # {
+    #   'tls-hw-tx-offload': {'fixed': True, 'on': False},
+    #   'tx-checksum-fcoe-crc': {'fixed': True, 'on': False},
+    #   'tx-checksum-ip-generic': {'fixed': False, 'on': True},
+    #   'tx-checksum-ipv4': {'fixed': True, 'on': False},
+    #   'tx-checksum-ipv6': {'fixed': True, 'on': False},
+    #   'tx-checksum-sctp': {'fixed': True, 'on': False},
+    #   'tx-checksumming': {'fixed': False, 'on': True},
+    #   'tx-esp-segmentation': {'fixed': True, 'on': False},
+    # }
+    features = { }
+
+    def __init__(self, ifname):
+        # Now populate features dictionaty
+        tmp = cmd(f'ethtool -k {ifname}')
+        # skip the first line, it only says: "Features for eth0":
+        for line in tmp.splitlines()[1:]:
+            if ":" in line:
+                key, value = [s.strip() for s in line.strip().split(":", 1)]
+                fixed = "fixed" in value
+                if fixed:
+                    value = value.split()[0].strip()
+                self.features[key.strip()] = {
+                    "on": value == "on",
+                    "fixed": fixed
+                }
+
+    def is_fixed_lro(self):
+        # in case of a missing configuration, rather return "fixed". In Ethtool
+        # terminology "fixed" means the setting can not be changed by the user.
+        return self.features.get('large-receive-offload', True).get('fixed', True)
+
+    def is_fixed_gro(self):
+        # in case of a missing configuration, rather return "fixed". In Ethtool
+        # terminology "fixed" means the setting can not be changed by the user.
+        return self.features.get('generic-receive-offload', True).get('fixed', True)
+
+    def is_fixed_gso(self):
+        # in case of a missing configuration, rather return "fixed". In Ethtool
+        # terminology "fixed" means the setting can not be changed by the user.
+        return self.features.get('generic-segmentation-offload', True).get('fixed', True)
+
+    def is_fixed_sg(self):
+        # in case of a missing configuration, rather return "fixed". In Ethtool
+        # terminology "fixed" means the setting can not be changed by the user.
+        return self.features.get('scatter-gather', True).get('fixed', True)
+
+    def is_fixed_tso(self):
+        # in case of a missing configuration, rather return "fixed". In Ethtool
+        # terminology "fixed" means the setting can not be changed by the user.
+        return self.features.get('tcp-segmentation-offload', True).get('fixed', True)
+
+    def is_fixed_ufo(self):
+        # in case of a missing configuration, rather return "fixed". In Ethtool
+        # terminology "fixed" means the setting can not be changed by the user.
+        return self.features.get('udp-fragmentation-offload', True).get('fixed', True)
