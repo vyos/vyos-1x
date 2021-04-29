775eaa3f20c56a357130e8c08ec2c35033db8fd9
sever-sever <v.gletenko@vyos.io>
2021-04-29 08:22:01 +0000

    pppoe: T3250: Not mangle values for tag nodes
    (cherry picked from commit 80fe3ef4d49f6ee277d52ef5847f011e5775e7fb)

--- python/vyos/configdict.py
+++ python/vyos/configdict.py
@@ -499,7 +499,7 @@ def get_accel_dict(config, base, chap_secrets):
     from vyos.util import get_half_cpus
     from vyos.template import is_ipv4
 
-    dict = config.get_config_dict(base, key_mangling=('-', '_'), get_first_key=True)
+    dict = config.get_config_dict(base, key_mangling=('-', '_'), get_first_key=True, no_tag_node_value_mangle=True)
 
     # We have gathered the dict representation of the CLI, but there are default
     # options which we need to update into the dictionary retrived.
