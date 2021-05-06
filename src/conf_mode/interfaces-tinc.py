f0db8f49571631ae042cd1c08116ebbbf933fdd7
jack9603301 <jack9603301@163.com>
2021-05-06 19:36:37 +0800

    tinc-vpn: T766: Fixed the style of the configurator

--- src/conf_mode/interfaces-tinc.py
+++ src/conf_mode/interfaces-tinc.py
@@ -18,7 +18,6 @@ import os
 import shutil
 
 from sys import exit
-from copy import deepcopy
 
 from netifaces import interfaces
 from vyos.configdict import get_interface_dict,leaf_node_changed
@@ -27,22 +26,16 @@ from time import sleep
 from vyos.ifconfig import TincIf
 from vyos import ConfigError
 from vyos.util import call
-from vyos.xml import defaults
-from vyos.configdict import dict_merge
 from vyos.template import render
 from vyos.configverify import verify_address
 from vyos.configverify import verify_dhcpv6
-from vyos.configverify import verify_interface_exists
-from vyos.configverify import verify_mtu
-from vyos.configverify import verify_mtu_ipv6
-from vyos.configverify import verify_vlan_config
 from vyos.configverify import verify_vrf
 
 from vyos import airbag
 
 airbag.enable()
 
-def get_config(config=None):
+def get_config(config = None):
     if config:
         conf = config
     else:
@@ -53,10 +46,10 @@ def get_config(config=None):
     tinc = get_interface_dict(conf, base)
     
     if 'network_dir' in tinc:
-        tinc_network_dir=tinc['network_dir']
+        tinc_network_dir = tinc['network_dir']
         ed25516_private_key = f'{tinc_network_dir}/ed25519_key.priv'
         rsa_private_key = f'{tinc_network_dir}/rsa_key.priv'
-        tinc.update({'ed25516_private_key':ed25516_private_key,'rsa_private_key':rsa_private_key});
+        tinc.update({'ed25516_private_key': ed25516_private_key, 'rsa_private_key': rsa_private_key});
     
     if 'deleted' in tinc:
         tmp = leaf_node_changed(conf, ['network-dir'])
@@ -81,10 +74,10 @@ def verify(tinc):
     if 'node_name' not in tinc:
         raise ConfigError('node_name must be set')
     if 'connect' in tinc:
-        node_name=tinc['node_name']
-        connect_peer_node_name=tinc['connect']
+        node_name = tinc['node_name']
+        connect_peer_node_name = tinc['connect']
         if node_name == connect_peer_node_name:
-            raise ConfigError('The local node name("{local_node}") and the remote target connection node name("{remote_node}") cannot match'.format(local_node=node_name,remote_node=connect_peer_node_name))
+            raise ConfigError('The local node name("{local_node}") and the remote target connection node name("{remote_node}") cannot match'.format(local_node = node_name, remote_node = connect_peer_node_name))
     
     if 'proxy' in tinc:
         if 'address' not in tinc['proxy']:
@@ -106,7 +99,7 @@ def verify(tinc):
     if 'connect' in tinc:
         for conn in tinc['connect']:
             if 'conf_path' in tinc['connect'][conn] and not os.path.exists(tinc['connect'][conn]['conf_path']):
-                raise ConfigError('The configuration file connected to peer {peer} is missing'.format(peer=conn))
+                raise ConfigError('The configuration file connected to peer {peer} is missing'.format(peer = conn))
     
     verify_dhcpv6(tinc)
     verify_address(tinc)
@@ -118,13 +111,13 @@ def verify(tinc):
     
     return None
 
-def create_or_delete_network(tinc,op):
+def create_or_delete_network(tinc, op):
     if tinc is None:
         return None
     interface = tinc['ifname']
     network = tinc['ifname']
-    system_network_dir=f'/etc/tinc/{network}'
-    tinc_network_dir=tinc['network_dir']
+    system_network_dir = f'/etc/tinc/{network}'
+    tinc_network_dir = tinc['network_dir']
     if tinc:
         if op == "delete":
             if os.path.exists(system_network_dir):
@@ -133,17 +126,17 @@ def create_or_delete_network(tinc,op):
                 shutil.rmtree(tinc_network_dir)
         elif op == 'create':
             if not os.path.exists(tinc_network_dir):
-                os.makedirs(tinc_network_dir, mode=0o666 )
+                os.makedirs(tinc_network_dir, mode = 0o666 )
             if not os.path.exists(f'{tinc_network_dir}/hosts'):
-                os.makedirs(f'{tinc_network_dir}/hosts', mode=0o666 )
+                os.makedirs(f'{tinc_network_dir}/hosts', mode = 0o666 )
             if not os.path.exists(system_network_dir):
-                os.makedirs(system_network_dir, mode=0o666 )
+                os.makedirs(system_network_dir, mode = 0o666 )
             call(f'chown vyos -R {tinc_network_dir}')
             call(f'chown vyos -R {system_network_dir}')
             call(f'chmod a+x -R {tinc_network_dir}')
             call(f'chmod a+x -R {system_network_dir}')
             if not os.path.exists(f'{system_network_dir}/hosts'):
-                os.symlink(f'{tinc_network_dir}/hosts',f'{system_network_dir}/hosts')
+                os.symlink(f'{tinc_network_dir}/hosts', f'{system_network_dir}/hosts')
     return None
 
 def generate(tinc):
@@ -153,24 +146,24 @@ def generate(tinc):
         return None
     interface = tinc['ifname']
     network = tinc['ifname']
-    node_name=tinc['node_name']
+    node_name = tinc['node_name']
     keys_length = tinc['keys_length']
-    tinc_network_dir=tinc['network_dir']
+    tinc_network_dir = tinc['network_dir']
     rsa_private_keyfile = f'{tinc_network_dir}/rsa_key.priv'
     ed25519_private_keyfile = f'{tinc_network_dir}/ed25519_key.priv'
-    tinc_hosts_dir=f'/{tinc_network_dir}/hosts'
-    system_network_dir=f'/etc/tinc/{network}'
-    tinc_main_config=f'{system_network_dir}/tinc.conf'
-    tinc_host_local_peer_config=f'{tinc_network_dir}/hosts/{node_name}'
+    tinc_hosts_dir = f'/{tinc_network_dir}/hosts'
+    system_network_dir = f'/etc/tinc/{network}'
+    tinc_main_config = f'{system_network_dir}/tinc.conf'
+    tinc_host_local_peer_config = f'{tinc_network_dir}/hosts/{node_name}'
     if tinc:
         if not os.path.exists(system_network_dir) or not os.path.exists(tinc_network_dir):
-            create_or_delete_network(tinc,'create') # Create a directory
+            create_or_delete_network(tinc, 'create') # Create a directory
         call(f'tinc -b -n {network} init {node_name}')
         if not os.path.exists(rsa_private_keyfile) and not os.path.exists(ed25519_private_keyfile) and not os.path.exists(tinc_host_local_peer_config) :
             render(tinc_host_local_peer_config, 'tinc/hosts_config.tmpl', tinc)
             call(f'tinc -b -n {network} generate-keys {keys_length}')
-            shutil.move(f'{system_network_dir}/ed25519_key.priv',f'{tinc_network_dir}/ed25519_key.priv')
-            shutil.move(f'{system_network_dir}/rsa_key.priv',f'{tinc_network_dir}/rsa_key.priv')
+            shutil.move(f'{system_network_dir}/ed25519_key.priv', f'{tinc_network_dir}/ed25519_key.priv')
+            shutil.move(f'{system_network_dir}/rsa_key.priv', f'{tinc_network_dir}/rsa_key.priv')
         elif not os.path.exists(rsa_private_keyfile) or not os.path.exists(ed25519_private_keyfile) or not os.path.exists(tinc_host_local_peer_config):
             if os.path.exists(rsa_private_keyfile):
                 os.remove(rsa_private_keyfile)
@@ -180,8 +173,8 @@ def generate(tinc):
                 os.remove(tinc_host_local_peer_config)
             render(tinc_host_local_peer_config, 'tinc/hosts_config.tmpl', tinc)
             call(f'tinc -b -n {network} generate-keys {keys_length}')
-            shutil.move(f'{system_network_dir}/ed25519_key.priv',f'{tinc_network_dir}/ed25519_key.priv')
-            shutil.move(f'{system_network_dir}/rsa_key.priv',f'{tinc_network_dir}/rsa_key.priv')
+            shutil.move(f'{system_network_dir}/ed25519_key.priv', f'{tinc_network_dir}/ed25519_key.priv')
+            shutil.move(f'{system_network_dir}/rsa_key.priv', f'{tinc_network_dir}/rsa_key.priv')
         else:
             try:
                 call(f'tinc -n {network} del subnet')
@@ -206,7 +199,7 @@ def generate(tinc):
                     if tinc['connect'][conn]['conf_path'] != conf and tinc['connect'][conn]['conf_path'] != system_conf:
                         base = tinc['connect'][conn]['conf_path']
                         tmp = f'/tmp/remote_tinc_{conn}'
-                        os.symlink(base,conf)
+                        os.symlink(base, conf)
             
     return None
 
@@ -218,7 +211,7 @@ def apply(tinc):
         network = tinc['ifname']
         call(f'systemctl stop tinc@{network}')
         if 'deleted' in tinc:
-            create_or_delete_network(tinc,'delete') # Delete a directory
+            create_or_delete_network(tinc, 'delete') # Delete a directory
         try:
             t = TincIf(interface)
             # Delete Interface
