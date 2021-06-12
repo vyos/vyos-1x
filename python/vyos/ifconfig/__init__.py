c2a1c071e7d0a9ca754d7f5016eed7db188b3d1a
Christian Poessinger <christian@poessinger.com>
2021-06-12 13:17:13 +0200

    wwan: T3620: rename "wirelessmodem wlm" interfaces to new wwan interface tree

--- python/vyos/ifconfig/__init__.py
+++ python/vyos/ifconfig/__init__.py
@@ -35,3 +35,4 @@ from vyos.ifconfig.tunnel import TunnelIf
 from vyos.ifconfig.wireless import WiFiIf
 from vyos.ifconfig.l2tpv3 import L2TPv3If
 from vyos.ifconfig.macsec import MACsecIf
+from vyos.ifconfig.wwan import WWANIf
---
+++ src/conf_mode/interfaces-wwan.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2020-2021 VyOS maintainers and contributors
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 or later as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+
+from sys import exit
+
+from vyos.config import Config
+from vyos.configdict import get_interface_dict
+from vyos.configverify import verify_interface_exists
+from vyos.configverify import verify_vrf
+from vyos.ifconfig import WWANIf
+from vyos.util import cmd
+from vyos.template import render
+from vyos import ConfigError
+from vyos import airbag
+airbag.enable()
+
+def get_config(config=None):
+    """
+    Retrive CLI config as dictionary. Dictionary can never be empty, as at least the
+    interface name will be added or a deleted flag
+    """
+    if config:
+        conf = config
+    else:
+        conf = Config()
+    base = ['interfaces', 'wwan']
+    wwan = get_interface_dict(conf, base)
+
+    return wwan
+
+def verify(wwan):
+    if 'deleted' in wwan:
+        return None
+
+    ifname = wwan['ifname']
+    if not 'apn' in wwan:
+        raise ConfigError(f'No APN configured for "{ifname}"!')
+
+    verify_interface_exists(ifname)
+    verify_vrf(wwan)
+
+    return None
+
+def generate(wwan):
+    return None
+
+def apply(wwan):
+    # we only need the modem number. wwan0 -> 0, wwan1 -> 1
+    modem = wwan['ifname'].replace('wwan','')
+    base_cmd = f'mmcli --modem {modem}'
+
+    w = WWANIf(wwan['ifname'])
+    if 'deleted' in wwan or 'disable' in wwan:
+        w.remove()
+        cmd(f'{base_cmd} --simple-disconnect')
+        return None
+
+    cmd(f'{base_cmd} --simple-connect=\"apn={wwan["apn"]}\"')
+    w.update(wwan)
+
+    return None
+
+if __name__ == '__main__':
+    try:
+        c = get_config()
+        verify(c)
+        generate(c)
+        apply(c)
+    except ConfigError as e:
+        print(e)
+        exit(1)
