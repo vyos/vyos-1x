3ae265b300544b855b1b32862c901045ee146934
Christian Poessinger <christian@poessinger.com>
2021-06-21 21:25:04 +0200

    smoketest: ensure /config is a directory

---
+++ smoketest/scripts/system/test_config_mount.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2021 VyOS maintainers and contributors
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 or later as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import unittest
+
+class TestConfigDir(unittest.TestCase):
+    def test_config_dir(self):
+        self.assertTrue(os.path.isdir('/config'))
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
+
