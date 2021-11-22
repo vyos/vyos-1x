114be88565cd09c0a7dcbe67a753d7d2834659b9
John Estabrook <jestabro@vyos.io>
2021-11-22 15:29:46 -0600

    graphql: T3993: refactor directive and mutation definitions
    (cherry picked from commit ef7f5ca2fd2c0113875dbd9143342e925cf00621)

--- src/services/api/graphql/graphql/directives.py
+++ src/services/api/graphql/graphql/directives.py
@@ -1,34 +1,37 @@
 from ariadne import SchemaDirectiveVisitor, ObjectType
 from . mutations import make_configure_resolver, make_config_file_resolver
 
-class ConfigureDirective(SchemaDirectiveVisitor):
+def non(arg):
+    pass
+
+class VyosDirective(SchemaDirectiveVisitor):
+    def visit_field_definition(self, field, object_type, make_resolver=non):
+        name = f'{field.type}'
+        # field.type contains the return value of the mutation; trim value
+        # to produce canonical name
+        name = name.replace('Result', '', 1)
+
+        func = make_resolver(name)
+        field.resolve = func
+        return field
+
+
+class ConfigureDirective(VyosDirective):
     """
     Class providing implementation of 'configure' directive in schema.
 
     """
     def visit_field_definition(self, field, object_type):
-        name = f'{field.type}'
-        # field.type contains the return value of the mutation; trim value
-        # to produce canonical name
-        name = name.replace('Result', '', 1)
+        super().visit_field_definition(field, object_type,
+                                       make_resolver=make_configure_resolver)
 
-        func = make_configure_resolver(name)
-        field.resolve = func
-        return field
-
-class ConfigFileDirective(SchemaDirectiveVisitor):
+class ConfigFileDirective(VyosDirective):
     """
     Class providing implementation of 'configfile' directive in schema.
 
     """
     def visit_field_definition(self, field, object_type):
-        name = f'{field.type}'
-        # field.type contains the return value of the mutation; trim value
-        # to produce canonical name
-        name = name.replace('Result', '', 1)
-
-        func = make_config_file_resolver(name)
-        field.resolve = func
-        return field
+        super().visit_field_definition(field, object_type,
+                                       make_resolver=make_config_file_resolver)
 
 directives_dict = {"configure": ConfigureDirective, "configfile": ConfigFileDirective}
