73ac36a4bea3ed84079dd0db6e17127498f8c7b7
erkin <e.altunbas@vyos.io>
2022-01-11 12:02:32 +0300

    remote: T3950: Gracefully handle chained exceptions

--- python/vyos/remote.py
+++ python/vyos/remote.py
@@ -74,21 +74,6 @@ class SourceAdapter(HTTPAdapter):
             num_pools=connections, maxsize=maxsize,
             block=block, source_address=self._source_pair)
 
-class WrappedFile:
-    def __init__(self, obj, size=None, chunk_size=CHUNK_SIZE):
-        self._obj = obj
-        self._progress = size and make_incremental_progressbar(chunk_size / size)
-    def read(self, size=-1):
-        if self._progress:
-            next(self._progress)
-        self._obj.read(size)
-    def write(self, size=-1):
-        if self._progress:
-            next(self._progress)
-        self._obj.write(size)
-    def __getattr__(self, attr):
-         return getattr(self._obj, attr)
-
 
 def check_storage(path, size):
     """
@@ -98,8 +83,7 @@ def check_storage(path, size):
     directory = path if os.path.isdir(path) else (os.path.dirname(os.path.expanduser(path)) or os.getcwd())
     # `size` can be None or 0 to indicate unknown size.
     if not size:
-        print_error('Warning: Cannot determine size of remote file.')
-        print_error('Bravely continuing regardless.')
+        print_error('Warning: Cannot determine size of remote file. Bravely continuing regardless.')
         return
 
     if size < 1024 * 1024:
@@ -242,7 +226,7 @@ class HttpC:
                 r.raise_for_status()
                 # If the request got redirected, keep the last URL we ended up with.
                 final_urlstring = r.url
-                if r.history:
+                if r.history and self.progressbar:
                     print_error('Redirecting to ' + final_urlstring)
                 # Check for the prospective file size.
                 try:
@@ -264,10 +248,9 @@ class HttpC:
                     shutil.copyfileobj(r.raw, f)
 
     def upload(self, location: str):
-        size = os.path.getsize(location) if self.progressbar else None
-        # Keep in mind that `data` can be a file-like or iterable object.
-        with self._establish() as s, file(location, 'rb') as f:
-            s.post(self.urlstring, data=WrappedFile(f, size), allow_redirects=True)
+        # Does not yet support progressbars.
+        with self._establish() as s, open(location, 'rb') as f:
+            s.post(self.urlstring, data=f, allow_redirects=True)
 
 
 class TftpC:
@@ -333,11 +316,12 @@ def friendly_download(local_path, urlstring, source_host='', source_port=0):
         sys.exit(1)
     except:
         import traceback
+        print_error(f'Failed to download {urlstring}.')
         # There are a myriad different reasons a download could fail.
         # SSH errors, FTP errors, I/O errors, HTTP errors (403, 404...)
         # We omit the scary stack trace but print the error nevertheless.
-        print_error(f'Failed to download {urlstring}.')
-        traceback.print_exception(*sys.exc_info()[:2], None)
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        traceback.print_exception(exc_type, exc_value, None, 0, None, False)
         sys.exit(1)
     else:
         print_error('Download complete.')
