3081705175da8eb4ee7c1264e64d06f30b175fdb
Christian Poessinger <christian@poessinger.com>
2022-04-15 19:42:22 +0200

    smoketest: salt: T4363: add initial testcase

---
+++ smoketest/scripts/cli/test_service_salt.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2022 VyOS maintainers and contributors
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 or later as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+
+from socket import gethostname
+from base_vyostest_shim import VyOSUnitTestSHIM
+
+from vyos.util import process_named_running
+from vyos.util import read_file
+
+PROCESS_NAME = 'salt-minion'
+SALT_CONF = '/etc/salt/minion'
+base_path = ['service', 'salt-minion']
+
+class TestServiceSALT(VyOSUnitTestSHIM.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super(cls, cls).setUpClass()
+
+        # ensure we can also run this test on a live system - so lets clean
+        # out the current configuration :)
+        cls.cli_delete(cls, base_path)
+
+    def tearDown(self):
+        # Check for running process
+        self.assertTrue(process_named_running(PROCESS_NAME))
+
+        # delete testing SALT config
+        self.cli_delete(base_path)
+        self.cli_commit()
+
+        self.assertFalse(process_named_running(PROCESS_NAME))
+
+    def test_default(self):
+        servers = ['192.0.2.1', '192.0.2.2']
+
+        for server in servers:
+            self.cli_set(base_path + ['master', server])
+
+        self.cli_commit()
+
+        # commiconf = read_file() Check configured port
+        conf = read_file(SALT_CONF)
+        self.assertIn(f'- {server}', conf)
+
+        # defaults
+        hostname = gethostname()
+        self.assertIn(f'hash_type: sha256', conf)
+        self.assertIn(f'id: {hostname}', conf)
+        self.assertIn(f'mine_interval: 60', conf)
+
+    def test_options(self):
+        server = '192.0.2.3'
+        hash = 'sha1'
+        id = 'foo'
+        interval = '120'
+
+        self.cli_set(base_path + ['master', server])
+        self.cli_set(base_path + ['hash', hash])
+        self.cli_set(base_path + ['id', id])
+        self.cli_set(base_path + ['interval', interval])
+
+        self.cli_commit()
+
+        # commiconf = read_file() Check configured port
+        conf = read_file(SALT_CONF)
+        self.assertIn(f'- {server}', conf)
+
+        # defaults
+        self.assertIn(f'hash_type: {hash}', conf)
+        self.assertIn(f'id: {id}', conf)
+        self.assertIn(f'mine_interval: {interval}', conf)
+        self.assertIn(f'id: {id}', conf)
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
