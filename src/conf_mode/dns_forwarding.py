ee603b3a0f9f3add72c1e5ac2277c013d40cf5a4
Christian Poessinger <christian@poessinger.com>
2022-07-07 12:36:34 +0200

    dns: T4509: improve 6to4 error message

--- src/conf_mode/dns_forwarding.py
+++ src/conf_mode/dns_forwarding.py
@@ -270,7 +270,7 @@ def verify(dns):
         dns_prefix = dns['dns64_prefix'].split('/')[1]
         # RFC 6147 requires prefix /96
         if int(dns_prefix) != 96:
-            raise ConfigError('DNS forwarding "dns64-prefix" must be /96')
+            raise ConfigError('DNS 6to4 prefix must be of length /96')
 
     if ('authoritative_zone_errors' in dns) and dns['authoritative_zone_errors']:
         for error in dns['authoritative_zone_errors']:
