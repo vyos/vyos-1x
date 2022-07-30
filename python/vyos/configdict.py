a295ec1fc4cb9cdbb0bd5efd7c8e72060ce036f5
Christian Poessinger <christian@poessinger.com>
2022-07-30 19:52:52 +0200

    bridge: T4565: is_member() must return the dict of the member interface
    ... otherwise functionality like bridge VLANs will loose configuration
    on membe rinterface update (e.g. description)

--- python/vyos/configdict.py
+++ python/vyos/configdict.py
@@ -222,8 +222,10 @@ def is_member(conf, interface, intftype=None):
         for intf in conf.list_nodes(base):
             member = base + [intf, 'member', 'interface', interface]
             if conf.exists(member):
-                if conf.exists(['interfaces', Section.section(interface), interface]):
-                    ret_val.update({intf : {}})
+                tmp = conf.get_config_dict(member, key_mangling=('-', '_'),
+                                           get_first_key=True,
+                                           no_tag_node_value_mangle=True)
+                ret_val.update({intf : tmp})
 
     return ret_val
 
