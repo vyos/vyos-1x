319d391bbd5e50078eabdf13ea75974cd0dc8c32
m.korobeinikov <m.korobeinikov@vyos.io>
2022-10-21 14:02:25 +1000

    T4767 : replace sh to Python
    refactoring and design improvement

--- src/op_mode/generate_ipsec_debug_archive.sh
+++ src/op_mode/generate_ipsec_debug_archive.sh
@@ -1,76 +1,36 @@
-#!/usr/bin/env python3
+#!/usr/bin/env bash
 
-from datetime import datetime
-from pathlib import Path
-from shutil import rmtree
-from socket import gethostname
-from sys import exit
-from tarfile import open as tar_open
+# Collecting IPSec Debug Information
 
-from vyos.util import rc_cmd
+DATE=`date +%d-%m-%Y`
 
-# define a list of commands that needs to be executed
-CMD_LIST: list[str] = [
-    'sudo ipsec status',
-    'sudo swanctl -L',
-    'sudo swanctl -l',
-    'sudo swanctl -P',
-    'sudo ip x sa show',
-    'sudo ip x policy show',
-    'sudo ip tunnel show',
-    'sudo ip address',
-    'sudo ip rule show',
-    'sudo ip route | head -100',
-    'sudo ip route show table 220'
-]
-JOURNALCTL_CMD: str = 'sudo journalctl -b -n 10000 /usr/lib/ipsec/charon'
+a_CMD=(
+       "sudo ipsec status"
+       "sudo swanctl -L"
+       "sudo swanctl -l"
+       "sudo swanctl -P"
+       "sudo ip x sa show"
+       "sudo ip x policy show"
+       "sudo ip tunnel show"
+       "sudo ip address"
+       "sudo ip rule show"
+       "sudo ip route"
+       "sudo ip route show table 220"
+      )
 
 
-# execute a command and save the output to a file
-def save_stdout(command: str, file: Path) -> None:
-    rc, stdout = rc_cmd(command)
-    body: str = f'''###
-Command: {command}
-Exit code: {rc}
-Stdout:
-{stdout}
-'''
-    with file.open(mode='a') as f:
-        f.write(body)
+echo "DEBUG: ${DATE} on host \"$(hostname)\"" > /tmp/ipsec-status-${DATE}.txt
+date >> /tmp/ipsec-status-${DATE}.txt
 
+# Execute all DEBUG commands and save it to file
+for cmd in "${a_CMD[@]}"; do
+    echo -e "\n### ${cmd} ###" >> /tmp/ipsec-status-${DATE}.txt
+    ${cmd} >> /tmp/ipsec-status-${DATE}.txt 2>/dev/null
+done
 
-# get local host name
-hostname: str = gethostname()
-# get current time
-time_now: str = datetime.now().isoformat(timespec='seconds')
+# Collect charon logs, build .tgz archive
+sudo journalctl /usr/lib/ipsec/charon > /tmp/journalctl-charon-${DATE}.txt && \
+sudo tar -zcvf /tmp/ipsec-debug-${DATE}.tgz /tmp/journalctl-charon-${DATE}.txt /tmp/ipsec-status-${DATE}.txt >& /dev/null
+sudo rm -f /tmp/journalctl-charon-${DATE}.txt /tmp/ipsec-status-${DATE}.txt
 
-# define a temporary directory for logs and collected data
-tmp_dir: Path = Path(f'/tmp/ipsec_debug_{time_now}')
-# set file paths
-ipsec_status_file: Path = Path(f'{tmp_dir}/ipsec_status.txt')
-journalctl_charon_file: Path = Path(f'{tmp_dir}/journalctl_charon.txt')
-archive_file: str = f'/tmp/ipsec_debug_{time_now}.tar.bz2'
-
-# create files
-tmp_dir.mkdir()
-ipsec_status_file.touch()
-journalctl_charon_file.touch()
-
-try:
-    # execute all commands
-    for command in CMD_LIST:
-        save_stdout(command, ipsec_status_file)
-    save_stdout(JOURNALCTL_CMD, journalctl_charon_file)
-
-    # create an archive
-    with tar_open(name=archive_file, mode='x:bz2') as tar_file:
-        tar_file.add(tmp_dir)
-
-    # inform user about success
-    print(f'Debug file is generated and located in {archive_file}')
-except Exception as err:
-    print(f'Error during generating a debug file: {err}')
-finally:
-    # cleanup
-    rmtree(tmp_dir)
-    exit()
+echo "Debug file is generated and located in /tmp/ipsec-debug-${DATE}.tgz"
