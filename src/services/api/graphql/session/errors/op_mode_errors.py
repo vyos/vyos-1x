2ac28861e2a94bef73713fbb78b00ec9beaf4189
Daniil Baturin <daniil@baturin.org>
2023-01-10 15:29:47 +0000

    opmode: T2546: add UnsupportedOperation to op mode errors

--- src/services/api/graphql/session/errors/op_mode_errors.py
+++ src/services/api/graphql/session/errors/op_mode_errors.py
@@ -4,12 +4,14 @@ op_mode_err_msg = {
     "UnconfiguredSubsystem": "subsystem is not configured or not running",
     "DataUnavailable": "data currently unavailable",
     "PermissionDenied": "client does not have permission",
-    "IncorrectValue": "argument value is incorrect"
+    "IncorrectValue": "argument value is incorrect",
+    "UnsupportedOperation": "operation is not supported (yet)",
 }
 
 op_mode_err_code = {
     "UnconfiguredSubsystem": 2000,
     "DataUnavailable": 2001,
     "PermissionDenied": 1003,
-    "IncorrectValue": 1002
+    "IncorrectValue": 1002,
+    "UnsupportedOperation": 1004,
 }
