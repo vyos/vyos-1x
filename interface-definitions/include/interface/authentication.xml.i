vyos/vyos-1x

commit e22e9c9210cb561fc6062ff5b1048e64cdc85c36

wwan: T3622: add constraint for username/password CLI nodes
- Username is up to 128 alphanumerical characters, -, _, #, and @
- Password is limited to ASCII characters only, with a total lenght of 128

interface-definitions/include/interface/authentication.xml.i
@@ -11,6 +11,10 @@
           <format>txt</format>
           <description>Username used for connection</description>
         </valueHelp>
+        <constraint>
+          <regex>[[:alnum:]][-_#@[:alnum:]]{0,127}</regex>
+        </constraint>
+        <constraintErrorMessage>Username is limited to alphanumerical characters, -, _, #, and @ with a total lenght of 128</constraintErrorMessage>
       </properties>
     </leafNode>
     <leafNode name="password">
@@ -20,6 +24,10 @@
           <format>txt</format>
           <description>Password used for connection</description>
         </valueHelp>
+        <constraint>
+          <regex>[[:ascii:]]{1,128}</regex>
+        </constraint>
+        <constraintErrorMessage>Password is limited to ASCII characters only, with a total lenght of 128</constraintErrorMessage>
       </properties>
     </leafNode>
   </children>

commit 945f27cd4c58fe23ac7a5a77c9af7fa4c223ff21

pppoe: wwan: T4998: fix username not accepting dot (.)
This fixes a regressin introduced in commit e22e9c9210cb5 ("wwan: T3622: add
constraint for username/password CLI nodes").

interface-definitions/include/interface/authentication.xml.i
@@ -12,9 +12,9 @@
           <description>Username</description>
         </valueHelp>
         <constraint>
-          <regex>[[:alnum:]][-_#@[:alnum:]]{0,127}</regex>
+          <regex>[[:ascii:]]{1,128}</regex>
         </constraint>
-        <constraintErrorMessage>Username is limited to alphanumerical characters, -, _, #, and @ with a total lenght of 128</constraintErrorMessage>
+        <constraintErrorMessage>Username is limited to ASCII characters only, with a total lenght of 128</constraintErrorMessage>
       </properties>
     </leafNode>
     <leafNode name="password">