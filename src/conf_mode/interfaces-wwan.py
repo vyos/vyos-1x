3de59f1365e5b908d3b24748d77dcb0bd9477ca8
Christian Breunig <christian@breunig.cc>
2023-07-27 11:18:11 +0200

    wwan: T3795: remove superfluous call to set_level()

--- src/conf_mode/interfaces-wwan.py
+++ src/conf_mode/interfaces-wwan.py
@@ -75,7 +75,6 @@ def get_config(config=None):
 
     # We need to know the amount of other WWAN interfaces as ModemManager needs
     # to be started or stopped.
-    conf.set_level(base)
     wwan['other_interfaces'] = conf.get_config_dict([], key_mangling=('-', '_'),
                                                        get_first_key=True,
                                                        no_tag_node_value_mangle=True)
