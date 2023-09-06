0cc75919dbaf2e1ec26885b7fbb7cc91dc7b28fa
John Estabrook <jestabro@vyos.io>
2023-09-06 13:37:34 -0500

    conf-mode: T5412: move dependency check from smoketest to nosetest
    (cherry picked from commit 12440ea1af8e60482a6a91c1cb04dcb86d7f4a68)

---
+++ src/tests/test_dependency_graph.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2023 VyOS maintainers and contributors
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 or later as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+from vyos.configdep import check_dependency_graph
+
+_here = os.path.dirname(__file__)
+ddir = os.path.join(_here, '../../data/config-mode-dependencies')
+
+from unittest import TestCase
+
+class TestDependencyGraph(TestCase):
+    def setUp(self):
+        pass
+
+    def test_acyclic(self):
+        res = check_dependency_graph(dependency_dir=ddir)
+        self.assertTrue(res)
