eac5251c4c804c580fe9f3c3d6c6f6e355fca6d1
Christian Breunig <christian@breunig.cc>
2023-12-19 07:49:03 +0100

    T2898: add ndp-proxy service
    VyOS CLI command
      set service ndp-proxy interface eth0 prefix 2001:db8::/64 mode 'static'
    
    Will generate the following NDP proxy configuration
    
      $ cat /run/ndppd/ndppd.conf
      # autogenerated by service_ndp-proxy.py
    
      # This tells 'ndppd' how often to reload the route file /proc/net/ipv6_route
      route-ttl 30000
    
      # This sets up a listener, that will listen for any Neighbor Solicitation
      # messages, and respond to them according to a set of rules
      proxy eth0 {
          # Turn on or off the router flag for Neighbor Advertisements
          router no
          # Control how long to wait for a Neighbor Advertisment message before invalidating the entry (milliseconds)
          timeout 500
          # Control how long a valid or invalid entry remains in the cache (milliseconds)
          ttl 30000
    
          # This is a rule that the target address is to match against. If no netmask
          # is provided, /128 is assumed. You may have several rule sections, and the
          # addresses may or may not overlap.
          rule 2001:db8::/64 {
              static
          }
      }
    
    (cherry picked from commit 4d721a58020971d00ab854c37b68e88359999f9c)

---
+++ smoketest/scripts/cli/test_service_ndp-proxy.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2023 VyOS maintainers and contributors
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 or later as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+
+from base_vyostest_shim import VyOSUnitTestSHIM
+
+from vyos.configsession import ConfigSessionError
+from vyos.ifconfig import Section
+from vyos.utils.process import cmd
+from vyos.utils.process import process_named_running
+
+PROCESS_NAME = 'ndppd'
+NDPPD_CONF = '/run/ndppd/ndppd.conf'
+base_path = ['service', 'ndp-proxy']
+
+def getConfigSection(string=None, end=' {', endsection='^}'):
+    tmp = f'cat {NDPPD_CONF} | sed -n "/^{string}{end}/,/{endsection}/p"'
+    out = cmd(tmp)
+    return out
+
+class TestServiceNDPProxy(VyOSUnitTestSHIM.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super(TestServiceNDPProxy, cls).setUpClass()
+
+        # ensure we can also run this test on a live system - so lets clean
+        # out the current configuration :)
+        cls.cli_delete(cls, base_path)
+
+    def tearDown(self):
+        # Check for running process
+        self.assertTrue(process_named_running(PROCESS_NAME))
+
+        # delete testing SSH config
+        self.cli_delete(base_path)
+        self.cli_commit()
+
+        self.assertFalse(process_named_running(PROCESS_NAME))
+
+    def test_basic(self):
+        interfaces = Section.interfaces('ethernet')
+        for interface in interfaces:
+            self.cli_set(base_path + ['interface', interface])
+            self.cli_set(base_path + ['interface', interface, 'enable-router-bit'])
+
+        self.cli_commit()
+
+        for interface in interfaces:
+            config = getConfigSection(f'proxy {interface}')
+            self.assertIn(f'proxy {interface}', config)
+            self.assertIn(f'router yes', config)
+            self.assertIn(f'timeout 500', config) # default value
+            self.assertIn(f'ttl 30000', config) # default value
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
