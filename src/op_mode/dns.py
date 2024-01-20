711de2bbb60f9741406e0fcffc5c7d8442128a02
Indrajit Raychaudhuri <irc@indrajit.com>
2024-01-19 23:47:46 -0600

    ddclient: T5966: Streamline dynamic dns op-mode configuration
    Update op-mode for dynamic dns to standardize on `vyos.opmode`. All
    methods of `op_mode/dns_dynamic.py` are now available in standardized
    `op_mode/dns.py`.
    
    Move op-mode command `update dns dynamic` to `reset dns dynamic` to
    reflect that it is not an update but a reset of the dynamic dns service.
    
    Also, make the help texts more consistent for all op-mode commands for
    `dns dynamic` and `dns forwarding`.
    
    (cherry picked from commit 2080f8edd69cdf7eed4bcfb739e68300faf0681f)
    
    # Conflicts:
    #	op-mode-definitions/dns-forwarding.xml.in
    #	src/op_mode/dns.py

--- src/op_mode/dns.py
+++ src/op_mode/dns.py
@@ -15,15 +15,44 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
+<<<<<<< HEAD
 import sys
+=======
+import os
+import sys
+import time
+import typing
+import vyos.opmode
+>>>>>>> 2080f8edd (ddclient: T5966: Streamline dynamic dns op-mode configuration)
 
 from tabulate import tabulate
 
 from vyos.configquery import ConfigTreeQuery
+<<<<<<< HEAD
 from vyos.utils.process import cmd
 
 import vyos.opmode
+=======
+from vyos.utils.process import cmd, rc_cmd
+from vyos.template import is_ipv4, is_ipv6
+>>>>>>> 2080f8edd (ddclient: T5966: Streamline dynamic dns op-mode configuration)
 
+_dynamic_cache_file = r'/run/ddclient/ddclient.cache'
+
+_dynamic_status_columns = {
+    'host':        'Hostname',
+    'ipv4':        'IPv4 address',
+    'status-ipv4': 'IPv4 status',
+    'ipv6':        'IPv6 address',
+    'status-ipv6': 'IPv6 status',
+    'mtime':       'Last update',
+}
+
+_forwarding_statistics_columns = {
+    'cache-entries':     'Cache entries',
+    'max-cache-entries': 'Max cache entries',
+    'cache-size':        'Cache size',
+}
 
 def _data_to_dict(data, sep="\t") -> dict:
     """
@@ -51,7 +80,53 @@ def _data_to_dict(data, sep="\t") -> dict:
             dictionary[key] = value
     return dictionary
 
+def _get_dynamic_host_records_raw() -> dict:
 
+    data = []
+
+    if os.path.isfile(_dynamic_cache_file): # A ddclient status file might not always exist
+        with open(_dynamic_cache_file, 'r') as f:
+            for line in f:
+                if line.startswith('#'):
+                    continue
+
+                props = {}
+                # ddclient cache rows have properties in 'key=value' format separated by comma
+                # we pick up the ones we are interested in
+                for kvraw in line.split(' ')[0].split(','):
+                    k, v = kvraw.split('=')
+                    if k in list(_dynamic_status_columns.keys()) + ['ip', 'status']:  # ip and status are legacy keys
+                        props[k] = v
+
+                # Extract IPv4 and IPv6 address and status from legacy keys
+                # Dual-stack isn't supported in legacy format, 'ip' and 'status' are for one of IPv4 or IPv6
+                if 'ip' in props:
+                    if is_ipv4(props['ip']):
+                        props['ipv4'] = props['ip']
+                        props['status-ipv4'] = props['status']
+                    elif is_ipv6(props['ip']):
+                        props['ipv6'] = props['ip']
+                        props['status-ipv6'] = props['status']
+                    del props['ip']
+
+                # Convert mtime to human readable format
+                if 'mtime' in props:
+                    props['mtime'] = time.strftime(
+                        "%Y-%m-%d %H:%M:%S", time.localtime(int(props['mtime'], base=10)))
+
+                data.append(props)
+
+    return data
+
+def _get_dynamic_host_records_formatted(data):
+    data_entries = []
+    for entry in data:
+        data_entries.append([entry.get(key) for key in _dynamic_status_columns.keys()])
+    header = _dynamic_status_columns.values()
+    output = tabulate(data_entries, header, numalign='left')
+    return output
+
+<<<<<<< HEAD
 def _get_raw_forwarding_statistics() -> dict:
     command = cmd('rec_control --socket-dir=/run/powerdns get-all')
     data = _data_to_dict(command)
@@ -70,6 +145,62 @@ def _get_formatted_forwarding_statistics(data):
     return output
 
 
+=======
+def _get_forwarding_statistics_raw() -> dict:
+    command = cmd('rec_control get-all')
+    data = _forwarding_data_to_dict(command)
+    data['cache-size'] = "{0:.2f} kbytes".format( int(
+        cmd('rec_control get cache-bytes')) / 1024 )
+    return data
+
+def _get_forwarding_statistics_formatted(data):
+    data_entries = []
+    data_entries.append([data.get(key) for key in _forwarding_statistics_columns.keys()])
+    header = _forwarding_statistics_columns.values()
+    output = tabulate(data_entries, header, numalign='left')
+    return output
+
+def _verify(target):
+    """Decorator checks if config for DNS related service exists"""
+    from functools import wraps
+
+    if target not in ['dynamic', 'forwarding']:
+        raise ValueError('Invalid target')
+
+    def _verify_target(func):
+        @wraps(func)
+        def _wrapper(*args, **kwargs):
+            config = ConfigTreeQuery()
+            if not config.exists(f'service dns {target}'):
+                _prefix = f'Dynamic DNS' if target == 'dynamic' else 'DNS Forwarding'
+                raise vyos.opmode.UnconfiguredSubsystem(f'{_prefix} is not configured')
+            return func(*args, **kwargs)
+        return _wrapper
+    return _verify_target
+
+@_verify('dynamic')
+def show_dynamic_status(raw: bool):
+    host_data = _get_dynamic_host_records_raw()
+    if raw:
+        return host_data
+    else:
+        return _get_dynamic_host_records_formatted(host_data)
+
+@_verify('dynamic')
+def reset_dynamic():
+    """
+    Reset Dynamic DNS cache
+    """
+    if os.path.exists(_dynamic_cache_file):
+        os.remove(_dynamic_cache_file)
+    rc, output = rc_cmd('systemctl restart ddclient.service')
+    if rc != 0:
+        print(output)
+        return None
+    print(f'Dynamic DNS state reset!')
+
+@_verify('forwarding')
+>>>>>>> 2080f8edd (ddclient: T5966: Streamline dynamic dns op-mode configuration)
 def show_forwarding_statistics(raw: bool):
 
     config = ConfigTreeQuery()
@@ -82,6 +213,31 @@ def show_forwarding_statistics(raw: bool):
     else:
         return _get_formatted_forwarding_statistics(dns_data)
 
+<<<<<<< HEAD
+=======
+@_verify('forwarding')
+def reset_forwarding(all: bool, domain: typing.Optional[str]):
+    """
+    Reset DNS Forwarding cache
+
+    :param all (bool): reset cache all domains
+    :param domain (str): reset cache for specified domain
+    """
+    if all:
+        rc, output = rc_cmd('rec_control wipe-cache ".$"')
+        if rc != 0:
+            print(output)
+            return None
+        print('DNS Forwarding cache reset for all domains!')
+        return output
+    elif domain:
+        rc, output = rc_cmd(f'rec_control wipe-cache "{domain}$"')
+        if rc != 0:
+            print(output)
+            return None
+        print(f'DNS Forwarding cache reset for domain "{domain}"!')
+        return output
+>>>>>>> 2080f8edd (ddclient: T5966: Streamline dynamic dns op-mode configuration)
 
 if __name__ == '__main__':
     try:
