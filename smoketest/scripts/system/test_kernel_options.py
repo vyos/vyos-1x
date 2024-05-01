6954288ea6b1ec7a64cec1d370f8b11884166c18
Christian Breunig <christian@breunig.cc>
2024-05-01 21:01:29 +0200

    smoketest: T6199: remove redundant code when unpacking Kernel GZ config
    (cherry picked from commit 6bcb201a0e7ee9fea5874b963bd3e727ecec578f)

--- smoketest/scripts/system/test_kernel_options.py
+++ smoketest/scripts/system/test_kernel_options.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright (C) 2020-2023 VyOS maintainers and contributors
+# Copyright (C) 2020-2024 VyOS maintainers and contributors
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 or later as
@@ -14,28 +14,38 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import gzip
 import re
 import os
 import platform
 import unittest
 
-from vyos.utils.process import call
-from vyos.utils.file import read_file
-
 kernel = platform.release()
-config = read_file(f'/boot/config-{kernel}')
-CONFIG = '/proc/config.gz'
 
 class TestKernelModules(unittest.TestCase):
     """ VyOS makes use of a lot of Kernel drivers, modules and features. The
     required modules which are essential for VyOS should be tested that they are
     available in the Kernel that is run. """
 
+    _config_data = None
+
+    @classmethod
+    def setUpClass(cls):
+        import gzip
+        from vyos.utils.process import call
+
+        super(TestKernelModules, cls).setUpClass()
+        CONFIG = '/proc/config.gz'
+
+        if not os.path.isfile(CONFIG):
+            call('sudo modprobe configs')
+
+        with gzip.open(CONFIG, 'rt') as f:
+            cls._config_data = f.read()
+
     def test_bond_interface(self):
         # The bond/lacp interface must be enabled in the OS Kernel
         for option in ['CONFIG_BONDING']:
-            tmp = re.findall(f'{option}=(y|m)', config)
+            tmp = re.findall(f'{option}=(y|m)', self._config_data)
             self.assertTrue(tmp)
 
     def test_bridge_interface(self):
@@ -43,7 +53,7 @@ class TestKernelModules(unittest.TestCase):
         for option in ['CONFIG_BRIDGE',
                        'CONFIG_BRIDGE_IGMP_SNOOPING',
                        'CONFIG_BRIDGE_VLAN_FILTERING']:
-            tmp = re.findall(f'{option}=(y|m)', config)
+            tmp = re.findall(f'{option}=(y|m)', self._config_data)
             self.assertTrue(tmp)
 
     def test_dropmon_enabled(self):
@@ -53,45 +63,52 @@ class TestKernelModules(unittest.TestCase):
             'CONFIG_BPF_EVENTS=y',
             'CONFIG_TRACEPOINTS=y'
         ]
-        if not os.path.isfile(CONFIG):
-            call('sudo modprobe configs')
 
-        with gzip.open(CONFIG, 'rt') as f:
-            config_data = f.read()
         for option in options_to_check:
-            self.assertIn(option, config_data,
-                          f"Option {option} is not present in /proc/config.gz")
+            self.assertIn(option, self._config_data)
 
     def test_synproxy_enabled(self):
         options_to_check = [
             'CONFIG_NFT_SYNPROXY',
             'CONFIG_IP_NF_TARGET_SYNPROXY'
         ]
-        if not os.path.isfile(CONFIG):
-            call('sudo modprobe configs')
-        with gzip.open(CONFIG, 'rt') as f:
-            config_data = f.read()
         for option in options_to_check:
-            tmp = re.findall(f'{option}=(y|m)', config_data)
+            tmp = re.findall(f'{option}=(y|m)', self._config_data)
             self.assertTrue(tmp)
 
     def test_qemu_support(self):
-        for option in ['CONFIG_VIRTIO_BLK', 'CONFIG_SCSI_VIRTIO',
-                       'CONFIG_VIRTIO_NET', 'CONFIG_VIRTIO_CONSOLE',
-                       'CONFIG_VIRTIO', 'CONFIG_VIRTIO_PCI',
-                       'CONFIG_VIRTIO_BALLOON', 'CONFIG_CRYPTO_DEV_VIRTIO',
-                       'CONFIG_X86_PLATFORM_DEVICES']:
-            tmp = re.findall(f'{option}=(y|m)', config)
+        options_to_check = [
+            'CONFIG_VIRTIO_BLK', 'CONFIG_SCSI_VIRTIO',
+            'CONFIG_VIRTIO_NET', 'CONFIG_VIRTIO_CONSOLE',
+            'CONFIG_VIRTIO', 'CONFIG_VIRTIO_PCI',
+            'CONFIG_VIRTIO_BALLOON', 'CONFIG_CRYPTO_DEV_VIRTIO',
+            'CONFIG_X86_PLATFORM_DEVICES'
+            ]
+        for option in options_to_check:
+            tmp = re.findall(f'{option}=(y|m)', self._config_data)
             self.assertTrue(tmp)
 
     def test_vmware_support(self):
         for option in ['CONFIG_VMXNET3']:
-            tmp = re.findall(f'{option}=(y|m)', config)
+            tmp = re.findall(f'{option}=(y|m)', self._config_data)
             self.assertTrue(tmp)
 
     def test_container_cgroup_support(self):
-        for option in ['CONFIG_CGROUPS', 'CONFIG_MEMCG', 'CONFIG_CGROUP_PIDS', 'CONFIG_CGROUP_BPF']:
-            tmp = re.findall(f'{option}=(y|m)', config)
+        options_to_check = [
+            'CONFIG_CGROUPS', 'CONFIG_MEMCG',
+            'CONFIG_CGROUP_PIDS', 'CONFIG_CGROUP_BPF'
+            ]
+        for option in options_to_check:
+            tmp = re.findall(f'{option}=(y|m)', self._config_data)
+            self.assertTrue(tmp)
+
+    def test_ip_routing_support(self):
+        options_to_check = [
+            'CONFIG_IP_ADVANCED_ROUTER', 'CONFIG_IP_MULTIPLE_TABLES',
+            'CONFIG_IP_ROUTE_MULTIPATH'
+            ]
+        for option in options_to_check:
+            tmp = re.findall(f'{option}=(y|m)', self._config_data)
             self.assertTrue(tmp)
 
     def test_vfio(self):
@@ -105,4 +122,3 @@ class TestKernelModules(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
-
