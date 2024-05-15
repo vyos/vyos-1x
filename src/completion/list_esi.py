c6be441c86bc8fe2e938e2bd3c85f99071cbfb49
l0crian1 <ryan.claridge13@gmail.com>
2024-05-15 14:54:18 -0400

    T6335: Add/Update EVPN op commands
    Added the following commands:
    show evpn
    show evpn es
    show evpn es <es-id>
    show evpn es detail
    show evpn es-evi
    show evpn es-evi detail
    show evpn es-evi vni <num>
    show evpn vni
    show evpn vni detail
    show evpn vni <num>
    
    Updated the following commands:
    show evpn access-vlan
    show evpn arp-cache
    show evpn mac
    show evpn next-hops
    show evpn rmac

---
+++ src/completion/list_esi.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2016-2024 VyOS maintainers and contributors
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 or later as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# This script is completion helper to list all configured VNIs that are visible to FRR
+
+import json
+from vyos.utils.process import cmd
+
+def get_esi():
+    esiDict = json.loads(cmd(f"vtysh -c 'show evpn es json'"))
+    esiList = []
+    for i in esiDict:
+        esiList.append(i['esi'])
+
+    print(' '.join(esiList))
+
+if __name__ == '__main__':
+    get_esi()
