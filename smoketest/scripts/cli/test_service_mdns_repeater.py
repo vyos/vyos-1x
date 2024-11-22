fbb2dcaae035186abc4745a9181c828466808760
Christian Breunig <christian@breunig.cc>
2024-11-22 20:34:12 +0100

    avahi: T6908: reduce runtime overhead in smoketests by using setUpClass/tearDownClass

--- smoketest/scripts/cli/test_service_mdns_repeater.py
+++ smoketest/scripts/cli/test_service_mdns_repeater.py
@@ -26,31 +26,39 @@ base_path = ['service', 'mdns', 'repeater']
 intf_base = ['interfaces', 'dummy']
 config_file = '/run/avahi-daemon/avahi-daemon.conf'
 
-
 class TestServiceMDNSrepeater(VyOSUnitTestSHIM.TestCase):
-    def setUp(self):
-        # Start with a clean CLI instance
-        self.cli_delete(base_path)
+    @classmethod
+    def setUpClass(cls):
+        super(TestServiceMDNSrepeater, cls).setUpClass()
 
-        # Service required a configured IP address on the interface
-        self.cli_set(intf_base + ['dum10', 'address', '192.0.2.1/30'])
-        self.cli_set(intf_base + ['dum10', 'ipv6', 'address', 'no-default-link-local'])
-        self.cli_set(intf_base + ['dum20', 'address', '192.0.2.5/30'])
-        self.cli_set(intf_base + ['dum20', 'address', '2001:db8:0:2::5/64'])
-        self.cli_set(intf_base + ['dum30', 'address', '192.0.2.9/30'])
-        self.cli_set(intf_base + ['dum30', 'address', '2001:db8:0:2::9/64'])
-        self.cli_set(intf_base + ['dum40', 'address', '2001:db8:0:2::11/64'])
-        self.cli_commit()
+        # ensure we can also run this test on a live system - so lets clean
+        # out the current configuration :)
+        cls.cli_delete(cls, base_path)
+
+        cls.cli_set(cls, intf_base + ['dum10', 'address', '192.0.2.1/30'])
+        cls.cli_set(cls, intf_base + ['dum10', 'ipv6', 'address', 'no-default-link-local'])
+        cls.cli_set(cls, intf_base + ['dum20', 'address', '192.0.2.5/30'])
+        cls.cli_set(cls, intf_base + ['dum20', 'address', '2001:db8:0:2::5/64'])
+        cls.cli_set(cls, intf_base + ['dum30', 'address', '192.0.2.9/30'])
+        cls.cli_set(cls, intf_base + ['dum30', 'address', '2001:db8:0:2::9/64'])
+        cls.cli_set(cls, intf_base + ['dum40', 'address', '2001:db8:0:2::11/64'])
+
+        cls.cli_commit(cls)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.cli_delete(cls, intf_base + ['dum10'])
+        cls.cli_delete(cls, intf_base + ['dum20'])
+        cls.cli_delete(cls, intf_base + ['dum30'])
+        cls.cli_delete(cls, intf_base + ['dum40'])
+
+        cls.cli_commit(cls)
 
     def tearDown(self):
         # Check for running process
         self.assertTrue(process_named_running('avahi-daemon'))
 
         self.cli_delete(base_path)
-        self.cli_delete(intf_base + ['dum10'])
-        self.cli_delete(intf_base + ['dum20'])
-        self.cli_delete(intf_base + ['dum30'])
-        self.cli_delete(intf_base + ['dum40'])
         self.cli_commit()
 
         # Check that there is no longer a running process
