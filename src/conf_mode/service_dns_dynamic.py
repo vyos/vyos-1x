be738c1a3106d0f16d8efc55f2cf1b217978676b
Indrajit Raychaudhuri <irc@indrajit.com>
2024-12-25 23:15:02 -0600

    ddclient: T6981: Enhance/add protocol configuration
    These new protocols are supported in ddclient v4:
    'ddns.fm', 'he.net', 'inwx'
    
    These existing protocols gained dual stack support
    in ddclient v4:
    'domeneshop', 'gandi', 'godaddy', 'mythicdyn',
    'noip', 'nsupdate', 'regfishde'

--- src/conf_mode/service_dns_dynamic.py
+++ src/conf_mode/service_dns_dynamic.py
@@ -38,18 +38,20 @@ zone_necessary = ['cloudflare', 'digitalocean', 'godaddy', 'hetzner', 'gandi',
 zone_supported = zone_necessary + ['dnsexit2', 'zoneedit1']
 
 # Protocols that do not require username
-username_unnecessary = ['1984', 'cloudflare', 'cloudns', 'digitalocean', 'dnsexit2',
-                        'duckdns', 'freemyip', 'hetzner', 'keysystems', 'njalla',
-                        'nsupdate', 'regfishde']
+username_unnecessary = ['1984', 'cloudflare', 'cloudns', 'ddns.fm', 'digitalocean',
+                        'dnsexit2', 'duckdns', 'freemyip', 'gandi', 'he.net',
+                        'hetzner', 'keysystems', 'njalla', 'nsupdate', 'regfishde']
 
 # Protocols that support TTL
 ttl_supported = ['cloudflare', 'dnsexit2', 'gandi', 'hetzner', 'godaddy', 'nfsn',
                  'nsupdate']
 
 # Protocols that support both IPv4 and IPv6
-dualstack_supported = ['cloudflare', 'digitalocean', 'dnsexit2', 'duckdns',
-                       'dyndns2', 'easydns', 'freedns', 'hetzner', 'infomaniak',
-                       'njalla']
+dualstack_supported = ['cloudflare', 'ddns.fm', 'digitalocean', 'dnsexit2',
+                       'domeneshop', 'duckdns', 'dyndns2', 'easydns', 'freedns',
+                       'gandi', 'godaddy', 'he.net', 'hetzner', 'infomaniak',
+                       'inwx', 'mythicdyn', 'njalla', 'noip', 'nsupdate',
+                       'regfishde']
 
 # dyndns2 protocol in ddclient honors dual stack for selective servers
 # because of the way it is implemented in ddclient
