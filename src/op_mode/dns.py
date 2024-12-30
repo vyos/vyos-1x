18a44a804ba0c4d70760d927e44b3ded0ab2524a
Indrajit Raychaudhuri <irc@indrajit.com>
2024-12-29 19:47:47 -0600

    ddclient: T6981: Remove processing legacy cache fields
    In ddclient v4, the 'ip' and 'status' fields are
    no longer supported. They were used to store IPv4
    addresses and their statuses in a backward compatible
    way in ddclient v3.

--- src/op_mode/dns.py
+++ src/op_mode/dns.py
@@ -24,7 +24,6 @@ import vyos.opmode
 from tabulate import tabulate
 from vyos.configquery import ConfigTreeQuery
 from vyos.utils.process import cmdl, rc_cmd
-from vyos.template import is_ipv4, is_ipv6
 
 _dynamic_cache_file = r'/run/ddclient/ddclient.cache'
 
@@ -84,19 +83,8 @@ def _get_dynamic_host_records_raw() -> dict:
                 # we pick up the ones we are interested in
                 for kvraw in line.split(' ')[0].split(','):
                     k, v = kvraw.split('=')
-                    if k in list(_dynamic_status_columns.keys()) + ['ip', 'status']:  # ip and status are legacy keys
+                    if k in list(_dynamic_status_columns.keys()):
                         props[k] = v
-
-                # Extract IPv4 and IPv6 address and status from legacy keys
-                # Dual-stack isn't supported in legacy format, 'ip' and 'status' are for one of IPv4 or IPv6
-                if 'ip' in props:
-                    if is_ipv4(props['ip']):
-                        props['ipv4'] = props['ip']
-                        props['status-ipv4'] = props['status']
-                    elif is_ipv6(props['ip']):
-                        props['ipv6'] = props['ip']
-                        props['status-ipv6'] = props['status']
-                    del props['ip']
 
                 # Convert mtime to human readable format
                 if 'mtime' in props:
