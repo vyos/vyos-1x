b9ec9b825811e35edc667b019e8189cdcad3a24b
Daniil Baturin <daniil@baturin.org>
2025-10-28 15:37:22 +0000

    Update src/conf_mode/service_dhcp-server.py
    Co-authored-by: Nataliia S. <81954790+natali-rs1985@users.noreply.github.com>

--- src/conf_mode/service_dhcp-server.py
+++ src/conf_mode/service_dhcp-server.py
@@ -309,10 +309,7 @@ def verify(dhcp):
                     # If a client class has been specified then it must exist
                     if 'client_class' in range_config:
                         client_class = range_config['client_class']
-                        if 'client_class' not in dhcp:
-                            raise ConfigError(f'Client class "{client_class}" set in range "{range}" but does not exist')
-
-                        if client_class not in dhcp['client_class'].keys():
+                       if client_class not in dhcp.get('client_class', {}):
                             raise ConfigError(f'Client class "{client_class}" set in range "{range}" but does not exist')
 
                     # Start/Stop address must be inside network
