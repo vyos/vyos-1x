922d515dccc5df1e94119839e9b1e56f3e6f1074
Christian Breunig <christian@breunig.cc>
2026-04-16 15:15:02 +0000

    op-mode: T8483: fix whitespace format issues for show_sensors.py

--- src/op_mode/show_sensors.py
+++ src/op_mode/show_sensors.py
@@ -28,22 +28,22 @@ if 'hypervisor' in cpu_info:
     print('VyOS running under hypervisor, no sensors available!')
     sys.exit(1)
 
-output,retcode = popen("sensors --no-adapter",  stderr=DEVNULL)
+output, retcode = popen("sensors --no-adapter", stderr=DEVNULL)
 if retcode == 0:
     print (output)
     sys.exit(0)
 else:
-    output,retcode = popen("sensors-detect --auto",stderr=DEVNULL)
-    match = re.search(r'#----cut here----(.*)#----cut here----',output, re.DOTALL)
+    output, retcode = popen("sensors-detect --auto", stderr=DEVNULL)
+    match = re.search(r'#----cut here----(.*)#----cut here----', output,
+                      re.DOTALL)
     if match:
         for module in match.group(0).split('\n'):
             if not module.startswith("#"):
                 popen("modprobe {}".format(module.strip()))
-                output,retcode = popen("sensors --no-adapter",  stderr=DEVNULL)
+                output, retcode = popen("sensors --no-adapter", stderr=DEVNULL)
                 if retcode == 0:
-                    print (output)
+                    print(output)
                     sys.exit(0)
 
-
-print ("No sensors found")
+print("No sensors found")
 sys.exit(1)
