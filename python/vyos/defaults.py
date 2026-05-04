8b2d2a80c0f9272d80ee4d90077978c9e43021b9
John Estabrook <jestabro@vyos.io>
2026-05-04 14:33:07 -0500

    T8502: add list of global config-sync exclude paths

--- python/vyos/defaults.py
+++ python/vyos/defaults.py
@@ -106,3 +106,7 @@ reference_tree_cache = '/usr/share/vyos/reftree.cache'
 activation_list = os.path.join(directories['config'], 'activation-list')
 activation_init = os.path.join(directories['data'], 'activation-init')
 activation_hint = os.path.join(directories['data'], '.activation_hint')
+
+config_sync_exclusion_list = os.path.join(
+    directories['data'], 'config-sync-exclude.json'
+)
