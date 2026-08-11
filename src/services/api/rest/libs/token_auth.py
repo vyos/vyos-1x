a4a52a0975ce4de70c43fa6cab1e09f55567dc35
omnom62 <75066712+omnom62@users.noreply.github.com>
2026-08-11 18:09:25 +1000

    http-api: T8989: Update src/services/api/rest/libs/token_auth.py
    Co-authored-by: Viacheslav Hletenko <seversss265@gmail.com>

--- src/services/api/rest/libs/token_auth.py
+++ src/services/api/rest/libs/token_auth.py
@@ -1,4 +1,4 @@
-# Copyright VyOS maintainers and contributors <maintainers@vyos.io>
+# Copyright (C) VyOS Inc.
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
