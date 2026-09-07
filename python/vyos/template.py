25699a8d9b0b3704aa24aed1b77917a6df6765b6
Andy Millett <amillett@ip-performance.co.uk>
2026-09-07 19:59:32 +0100

    dhcp: T7130: fix vendor option filter formatting

--- python/vyos/template.py
+++ python/vyos/template.py
@@ -1023,6 +1023,7 @@ def kea_dynamic_dns_update_domains(config, type_key):
 
     return dumps(out, indent=12)
 
+
 @register_filter('kea_vendor_option_defs')
 def kea_vendor_option_defs(config):
     from vyos.kea import kea_parse_vendor_option_defs
